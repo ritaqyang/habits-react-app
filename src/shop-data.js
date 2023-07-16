5a79dd5866757dcd87b13005296941e907b6c81b
ritaqyang <ritaqyang@gmail.com>
2023-07-16 01:18:03 -0400

    shop-data recover

--- src/shop-data.js
+++ src/shop-data.js
@@ -16,6 +16,54 @@ const SHOP_DATA = [
       },
     ],
   },
+
+  {
+    title: '2-YEAR-GOALS', 
+    items: [
+      {
+        id:10,
+        imageUrl: "https://i.ibb.co/0VLRmKJ/Mediamodifier-Design-Template-10.png" ,
+      },
+      {
+        id:11, 
+        imageUrl: "https://i.ibb.co/wrLk2qM/Mediamodifier-Design-Template-6.png", 
+
+      }, 
+      {
+        id: 12, 
+        imageUrl: "https://i.ibb.co/419wGtQ/Mediamodifier-Design-Template-5.png", 
+      }, 
+      {
+        id: 13, 
+        imageUrl:"https://i.ibb.co/r2jrZ35/Mediamodifier-Design-Template-7.png",
+      },
+
+    ]
+
+
+  }, 
+  {
+    title: "books", 
+    items: [
+      {
+        id: 20,
+        imageUrl: "https://dynamic.indigoimages.ca/v1/books/books/1451683405/1.jpg?width=614&maxheight=614&quality=85",
+      },
+      {
+        id: 21,
+        imageUrl: "https://dynamic.indigoimages.ca/v1/books/books/0060928417/1.jpg?width=614&maxheight=614&quality=85",
+      },
+      {
+        id: 22,
+        imageUrl: " https://dynamic.indigoimages.ca/v1/books/books/0679775439/1.jpg?width=614&maxheight=614&quality=85", 
+      },
+      {
+        id: 23,
+        imageUrl: " https://dynamic.indigoimages.ca/v1/books/books/0771009410/1.jpg?width=614&maxheight=614&quality=85",
+      },
+    ]
+
+  },
   
   
   
