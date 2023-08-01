20ef142b3c4b140f55bfee0039405e8a17083e45
ritaqyang <ritaqyang@gmail.com>
2023-08-01 16:30:56 -0400

    moving defaultresult into return statement

--- src/components/charts/investment-chart/investment-chart.component.jsx
+++ src/components/charts/investment-chart/investment-chart.component.jsx
@@ -15,27 +15,45 @@ const testdata= [15250, 41943, 77618, 116848, 159987, 207424];
   
 const defaultResult = [
   {
+    id:1, 
     name: "4%",
     type:"line",
     data: [15250, 41943, 77618, 116848, 159987, 207424]
   },
   
   {
+    id:2,
     name: "5%",
     type:"column",
     data: [15250, 43261, 82226, 127200, 179107, 239017]
   },
   {
+    id:3,
     name: "5%",
     type:"line",
     data: [15250, 43261, 82226, 127200, 179107, 239017]
   },
   {
+    id:4,
     name: "6%",
       
     data: [15250, 44630,87186, 138736, 201183,276828]
   }
 ]; 
+
+const calculateReturns = (initialInvestment, yearlyReturnRate, monthlyContribution, numOfMonths) => {
+
+  const P = monthlyContribution; 
+  const n = numOfMonths; 
+  const r = Math.pow(1 + yearlyReturnRate, 1 / 12) - 1;
+  const FV =  P * (Math.pow(1 + r, n )/r);
+
+  const initialInvestmentReturns = initialInvestment * ((1+r) ** n ); 
+
+  return initialInvestmentReturns + FV;
+}; 
+
+
 
 
 
@@ -72,14 +90,42 @@ const InvestChart = () => {
 
       const [category, setCategory] = useState(testcategory); 
       const [data, setData] = useState(testdata); 
-
       const [result, setResult] = useState(defaultResult); 
-
-      
+      const [maxValue, setMaxValue] = useState(240000);       
       
       const [formFields, setFormFields] = useState(defaultFormFields);
       const { monthlyAmount, savingsAmount } = formFields;
+       const getResultWithUserInput = (monthly, initial, yearlyInterest) => {
+        const array4 = []; 
+        //get array for 4% return 
       
+        for (let m = 0; m < 6; m++) {
+          array4.push(calculateReturns(initial,yearlyInterest, monthly, 60 * m)); 
+        }
+      
+        return array4; 
+      }; 
+
+
+      const temp = getResultWithUserInput(100,0,0.04); 
+      console.log("temp is" + temp); 
+
+      
+      const updateSubField = (objectID, newSubFieldValue) => {
+        const objectIndex = result.findIndex((obj) => obj.id === objectID);
+        console.log("index is " + objectIndex); 
+      
+        if (objectIndex === -1) return;
+      
+        const updatedObject = { ...result[objectIndex], data: newSubFieldValue };
+      
+        const updatedArray = [...result];
+        updatedArray[objectIndex] = updatedObject;
+      
+        setResult(updatedArray); 
+      };
+
+
       const resetFormFields = () => {
         setFormFields(defaultFormFields);
       };
@@ -99,7 +145,6 @@ const InvestChart = () => {
     
       const handleChange = (event) => {
         const { name, value } = event.target;
-    
         setFormFields({ ...formFields, [name]: value });
       };
   
@@ -136,23 +181,16 @@ const InvestChart = () => {
           yaxis: {
             tickAmount: 5,
             min: 0,
-            max: 240000,
+            max: maxValue,
           }
         }} 
-        // series={[{
-        //   name: 'series-1',
-        //   data: data
-        //   },
-        //   {
-        //     name: 'series-2',
-        //     data: [15250, 43261, 82226, 127200, 179107, 239017]
-        //   }
-        // ]}
-        series= {defaultResult}
-        
+      
+        series= {result}
         width={800} 
         height={500} 
       />
+
+      <Button onClick={updateSubField(0,getResultWithUserInput(100,0,0.04))}>click </Button>
       
  
     </>
