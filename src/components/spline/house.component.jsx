7578df7fc35ab7ff9f20c2f1e799029fe4431cf8
ritaqyang <ritaqyang@gmail.com>
2023-08-25 14:39:14 -0400

    migrate project styling using tailwind+ clean up

--- src/components/spline/house.component.jsx
+++
@@ -1,7 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function House() {
-  return (
-    <Spline scene="https://prod.spline.design/ry78516MLGoZnnDM/scene.splinecode" />
-  );
-}
